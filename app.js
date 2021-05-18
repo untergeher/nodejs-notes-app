6d76d06685f83151b9dff22d67be9e1dd91f66d8
Metin Cavdar <MCavdar@rockwellautomation.com>
2021-05-19 00:19:04 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const chalk = require('chalk')
+const { argv } = require('yargs')
+const yargs = require('yargs')
+
+const notes = require('./notes.js')
+
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command : 'add',
+    describe: 'Add a new note',
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'            
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler(argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
+
+//create remove command
+yargs.command({
+    command : 'remove',
+    describe : 'Remove a note',
+    builder:{
+        title:{
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title)
+    }
+})
+
+//create list command
+yargs.command({
+    command:'list',
+    describe:'List the notes',
+    handler(){
+        notes.listNotes()
+    }
+})
+
+//create read command
+
+yargs.command({
+    command:'read',
+    describe:'Read the note',
+    builder:{
+        title:{
+            describe: 'note title',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler(argv){
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
---
+++ test.js
@@ -0,0 +1,4 @@
+const myArray = [{title:'a1',body:'b1'},{title:'a2',body:'b2'}]
+const findArray = myArray.find((element)=>element.title === 'a1')
+
+console.log(findArray)
