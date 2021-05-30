fbd6517b659c6953395a6754dcc0e86d125b8cd8
Metin Cavdar <MCavdar@rockwellautomation.com>
2021-05-30 09:38:15 +0300

    prettier

--- notes.js
+++ notes.js
@@ -3,81 +3,76 @@ const chalk = require('chalk')
 
 const getNotes = () => 'Your Notes...'
 
-const addNote = (title,body) => {
-    const notes = loadNotes()
+const addNote = (title, body) => {
+  const notes = loadNotes()
 
-    const duplicatedNotes = notes.filter((note) => note.title === title)
+  const duplicatedNotes = notes.filter((note) => note.title === title)
 
-    debugger
+  debugger
 
-    if(duplicatedNotes.length === 0){
-        notes.push({
-            title: title,
-            body: body
-        })
-    
-        saveNote(notes)
-        console.log('New note added')
-    } else{
-        console.log('No note taken')
-    }
+  if (duplicatedNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    })
 
+    saveNote(notes)
+    console.log('New note added')
+  } else {
+    console.log('No note taken')
+  }
 }
 
 const saveNote = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json',dataJSON)
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
 }
 
 const loadNotes = () => {
-    try{
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = JSON.parse(dataBuffer)
-        return dataJSON
-    }
-    catch(e){
-        return []
-    }  
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = JSON.parse(dataBuffer)
+    return dataJSON
+  } catch (e) {
+    return []
+  }
 }
 
 const removeNote = (title) => {
-    const notes = loadNotes()
-    const filteredNotes = notes.filter((note) => note.title !== title)
+  const notes = loadNotes()
+  const filteredNotes = notes.filter((note) => note.title !== title)
 
-    if(filteredNotes.length !== notes.length){
-        console.log(chalk.bgGreen('Note removed!'))
-    }else{
-        console.log(chalk.bgRed('No Note removed!'))
-    }
+  if (filteredNotes.length !== notes.length) {
+    console.log(chalk.bgGreen('Note removed!'))
+  } else {
+    console.log(chalk.bgRed('No Note removed!'))
+  }
 
-    saveNote(filteredNotes)
+  saveNote(filteredNotes)
 }
 
 const listNotes = () => {
-    const notes = loadNotes()
-    console.log(chalk.green('Your Notes:'))
-    notes.forEach((note)=>console.log(note.title))
+  const notes = loadNotes()
+  console.log(chalk.green('Your Notes:'))
+  notes.forEach((note) => console.log(note.title))
 }
 
-const readNote = (title) =>{
+const readNote = (title) => {
+  const notes = loadNotes()
+  const noteToRead = notes.find((note) => note.title === title)
 
-    const notes = loadNotes()
-    const noteToRead = notes.find((note)=> note.title === title)
-
-    if(noteToRead){
-        console.log(chalk.inverse(noteToRead.title))
-        console.log(noteToRead.body)
-    }else{
-        console.log(chalk.red('No Note Found'))
-    }
-
-
+  if (noteToRead) {
+    console.log(chalk.inverse(noteToRead.title))
+    console.log(noteToRead.body)
+  } else {
+    console.log(chalk.red('No Note Found'))
+  }
 }
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote,
-    removeNote: removeNote,
-    listNotes: listNotes,
-    readNote:readNote
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
 }
